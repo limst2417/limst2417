42405457e4e76cf5808c472ce934666c16394bbe
limst2417 <162926194+limst2417@users.noreply.github.com>
2024-04-17 10:51:46 +0900

    Add files via upload

---
+++ 6wNo.4.py
@@ -0,0 +1,6 @@
+inFp=open("C:/Temp/data1.txt","r")
+
+inStr=inFp.readline()
+print(inStr,end="")
+
+inFp.close()
---
+++ 6wNo.6.py
@@ -0,0 +1,9 @@
+inFp=open("C:/Windows/win.ini","r")
+outFp=open("C:/Temp/data3.txt","w")
+
+inList=inFp.readlines()
+for inStr in inList:
+    outFp.writelines(inStr)
+
+inFp.close()
+outFp.close()
